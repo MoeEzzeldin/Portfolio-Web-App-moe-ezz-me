8a33ae555af1f94f6d5d2d977ea487a9b31b8197
Moe <ahmed.fakher.e@gmail.com>
2024-10-07 00:03:16 -0400

    finaizing

--- src/components/El-Pitch.vue
+++ src/components/El-Pitch.vue
@@ -1,30 +1,26 @@
 <template>
-    <section id="about">
-      <div class="img">
-        <img src="../assets/moe.jpg" alt="">
-      </div>
-      <p>
-        Technology and computers have been my passion since I was young, but my journey with tech
-        started back in 2010 when I got involved with the Arab Digital Expression Foundation
-        <a :href="deca" target="_blank">(ADEF)</a> and attended workshops on Linux and Ubuntu.
-        Through those courses, I wrote my first lines of command and. After immigrating to the US, I
-        focused on expanding my skills with Microsoft and Adobe apps and gaining leadership skills
-        while working customer service roles. I made wonderful connections and learned a lot about
-        community dynamics, but deep down I knew I wanted to follow my passion into a tech career
-        and start learning to code. After dabbling in HTML, CSS, and JavaScript through a 6-month
-        virtual front-end development program with Egypt Future Work Digital (FWD), I stumbled
-        across Tech Elevator
-        <a href="https://www.techelevator.com/" target="_blank">Tech Elevator</a>. and enrolled for
-        the Spring 2024 in-person Columbus cohort. Participating in Tech Elevator was a
-        game-changer. Not only did I spend 800+ hours coding and learning the languages and
-        frameworks necessary to succeed as a software developer, but I also made lifelong
-        connections and expanded my knowledge base beyond what I would have thought possible in 14
-        weeks. Since graduating in April 2024, my main focus has been learning more about network
-        layers and building a deeper understanding of DevOps for developing accessible scalable
-        applications. When I’m not coding, I’m usually hanging out with my wife and daughter,
-        playing guitar or video games, walking my dog, or catching Pokémon!
-      </p>
-    </section>
+  <section id="about">
+    <div class="img">
+      <img src="../assets/moe.jpg" alt="" />
+    </div>
+    <p>
+      Technology and computers have been my passion since I was young, but my journey with tech
+      started back in 2010 when I got involved with the
+      <b>Arab Digital Expression Foundation</b> (<a :href="deca" target="_blank">ADEF</a>) and
+      attended workshops on Linux and Ubuntu. Through those courses, I wrote my first command
+      lines.<br />
+      <br />
+      After immigrating to the US, I started learning HTML, CSS, and JavaScript with
+      <strong>Egypt Future Work Digital</strong> ( <a href="https://egfwd.com/">FWD</a> ), and enrolled for <a href="https://www.techelevator.com/" target="_blank">Tech Elevator</a> 
+        in-person Spring 2024 cohort. I spend over 800+ hours coding and learning the languages and frameworks
+      necessary for software development. Since graduating in April 2024, my main focus
+      has been learning more about network layers and building more understanding of DevOps for
+      developing accessible scalable applications.<br />
+      <br />
+      When I’m not coding, I’m usually hanging out with my wife and daughter, playing guitar or
+      video games, walking my dog, or catching Pokémons!
+    </p>
+  </section>
 </template>
 
 <script>
@@ -43,7 +39,6 @@ export default {
 </script>
 
 <style scoped>
-
 #about {
   display: flex;
   flex-direction: column;
@@ -51,9 +46,9 @@ export default {
   align-items: center;
   gap: 3rem;
   top: 5rem;
-  height: 100vh;
-  font-size: var(--font-large);
+  font-size: 1.1rem;
   font-weight: 400;
+  line-height: 1.6;
 }
 
 .img {
@@ -71,8 +66,6 @@ img {
   box-shadow: var(--inner-shadow);
   border-radius: 50%;
   padding: 10px;
-  filter: grayscale(50px);
+  filter: grayscale();
 }
-
-
 </style>
